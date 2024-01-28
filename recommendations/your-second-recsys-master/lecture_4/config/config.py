0009bdbc24c00fd0143ca27b70e08e8784b2f2df
Insaf Ashrapov <iashrapov@gmail.com>
2024-01-28 22:39:09 +0300

    course code

--- recommendations/your-second-recsys-master/lecture_4/config/config.py
+++
@@ -1,9 +0,0 @@
-import yaml
-from pathlib import Path
-
-config_path = Path(__file__).parent.joinpath("config.yaml").resolve()
-with open(config_path, "r") as f:
-    config = yaml.safe_load(f)
-
-recommender_conf = config["recommender"]
-path_conf = config["paths"]
